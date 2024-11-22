76fa4f3cfc276f1b45d31ff74f5ab8fffd00a4bd
Joslin Ishimwe <166525868+ishjosl@users.noreply.github.com>
2024-11-22 12:17:04 -0600

    Time: 3 ms (48.21%), Space: 18.9 MB (61.81%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,26 +1,21 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         
-        #intervals = [[1,3],[6,9]] newInterval = [2,5]
-        #[...] [.......] [.....] [...]
-                #[s....e]
-            
-        res =[]
-        ind = 0
-        len_intervals= len(intervals)
-        start, end = newInterval[0], newInterval[1]
+        #intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+        start = newInterval[0]
+        end = newInterval[1]
+        res= []
         
-        while ind < len(intervals) and start > intervals[ind][1]:
-            res.append(intervals[ind])
-            ind +=1
-        while ind < len(intervals) and end >= intervals[ind][0]:
-            start = min(start, intervals[ind][0])
-            end = max(end, intervals[ind][1])
-            ind +=1
+        i = 0
+        while i < len(intervals) and start > intervals[i][1]:
+            res.append(intervals[i])
+            i +=1
+        while i < len(intervals) and end >= intervals[i][0]:
+            start = min(start, intervals[i][0])
+            end = max(end, intervals[i][1])
+            i +=1
         res.append([start, end])
-        
-        while ind < len(intervals):
-            res.append(intervals[ind])
-            ind +=1
-            
+        while i < len(intervals):
+            res.append(intervals[i])
+            i +=1
         return res
